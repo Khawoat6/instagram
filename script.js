5ba812f04e7a39260ceed8013e398c1421cf5d9e
oat phattaraphon <phattaraphon.c@ku.th>
2020-09-05 21:47:13 +0700

    fetch image from unslash complete

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+function appendImageElement(keyword, index) {
+  const imgElement = document.createElement('img');
+  imgElement.src = `https://source.unsplash.com/400x225/?${keyword}&sig=${index};`;
+
+  const galleryElement = document.querySelector('.gallery');
+  galleryElement.appendChild(imgElement);
+}
+
+function run() {
+  appendImageElement('dog', 1);
+}
+
+run();
